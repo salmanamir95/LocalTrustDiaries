98ce1382f4254c3fbbff64fee6fb7cb04a01748b
Muhammad Salman Amir <mmsa155j3@gmail.com>
2026-05-13 21:57:11 +0500

    implementation changes because of parent git

--- TrustNode/TrustNode_ops.h
+++ TrustNode/TrustNode_ops.h
@@ -1,4 +1,7 @@
 #ifndef TRUST_NODE_H
+
+#include "LocalTrustDiaries/TrustNode.h"
+
 #define TRUST_NODE_H
 
 #if !defined(__linux__)
@@ -12,24 +15,18 @@
 #define TRUST_NODE_INLINE inline __attribute__((always_inline))
 
 
-typedef struct __attribute__((aligned(32))) {
-    uint8_t nodeid;
-    uint32_t trust;
-    uint32_t acks;
-    uint32_t nacks;
-    uint8_t reserved[19];
-} TrustNode;
+
 
 TRUST_NODE_INLINE void trust_node_init(TrustNode *&node){
-    node->nodeid = 0;
+    memset(&node->nodeid, 0, sizeof(ipv6_t));
     node->trust = 20;
     node->acks = 0;
     node->nacks = 0;
     memset(node->reserved, 0, sizeof(node->reserved));
 }
 
-TRUST_NODE_INLINE const uint8_t trust_node_get_id(const TrustNode *&node){
-    return node->nodeid;
+TRUST_NODE_INLINE const ipv6_t* trust_node_get_id(const TrustNode *&node){
+    return &node->nodeid;
 }
 
 TRUST_NODE_INLINE const uint32_t* trust_node_get_trust(const TrustNode *&node){
@@ -44,8 +41,8 @@ TRUST_NODE_INLINE const uint32_t* trust_node_get_nacks(const TrustNode *&node){
     return &node->nacks;
 }
 
-TRUST_NODE_INLINE void trust_node_set_id(TrustNode *&node, const uint8_t address){
-    node->nodeid = address;
+TRUST_NODE_INLINE void trust_node_set_id(TrustNode *&node, const ipv6_t* address){
+    node->nodeid = *address;
 }
 
 TRUST_NODE_INLINE void trust_node_set_trust(TrustNode *&node, const uint32_t trust){
--- Types/AckType.h
+++
@@ -1,7 +0,0 @@
-#ifndef ACK_TYPE_H
-#define ACK_TYPE_H
-
-#define ACK 0
-#define NACK 1
-
-#endif
